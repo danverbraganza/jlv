fb94d54536330fddb1b887e8a1e69e4b612b132e
Danver Braganza <danverbraganza@gmail.com>
2025-11-20 21:46:58 -0800

    Add command line parsing to jlv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::{Parser, command};
+
+#[derive(Parser, Debug)]
+#[command(name = "jlv", version, about = "JsonL viewer", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    debug: bool,
+
+    #[arg(value_name = "FILENAME")]
+    filename: Option<String>,
+
+    /// Or as flag: `jlv view -f myfile`
+    #[arg(short = 'f', long = "filename", value_name = "FILENAME")]
+    filename_flag: Option<String>,
+}
+
 fn main() {
-    println!("jlv exiting normally");
+    let cli = Cli::parse();
+
+    if cli.debug {
+        eprintln!("Debug mode on");
+    }
+
+    let filename = cli.filename.or(cli.filename_flag);
+
+    match filename {
+        Some(fname) => start_view(&fname),
+        None => {
+            eprintln!("Error: No filename provided.");
+            std::process::exit(1);
+        }
+    }
+}
+
+// Initializes the TUI view to view a given filename
+fn start_view(filename: &str) {
+    println!("Opening filename {filename}")
 }
