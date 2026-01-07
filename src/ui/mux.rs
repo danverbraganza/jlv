24092a2a79cb329db8b2bbec6c39e083911b8c15
Danver Braganza <danver@imbue.com>
2026-01-07 14:30:48 -0800

    Allow you to jump forward and back

--- src/ui/mux.rs
+++ src/ui/mux.rs
@@ -12,6 +12,11 @@ pub struct Mux {
     current_index: i16,
 }
 
+pub enum TabDirection {
+    Forward,
+    Backward,
+}
+
 impl Mux {
     pub fn new(table_view: TableView) -> Self {
         Self {
@@ -27,8 +32,12 @@ impl Mux {
                 code: crossterm::event::KeyCode::Enter,
                 ..
             } => {
-                let selected_record = self.table_view.selected_record();
-                self.add_detail_view(selected_record);
+                if self.on_table() {
+                    let selected_record = self.table_view.selected_record();
+                    self.add_detail_view(selected_record);
+                } else {
+                    self.close_current_tab()
+                }
             }
 
             _ => self.table_view.handle_keypress(key),
@@ -40,12 +49,37 @@ impl Mux {
         self.current_index = self.possible_tabs.len() as i16 - 1
     }
 
+    pub fn close_current_tab(&mut self) {
+        match self.current_index.into() {
+            Some(delindex) if delindex > -1 => {
+                self.possible_tabs.remove(delindex as usize);
+                self.current_index -= 1
+            }
+            _ => (),
+        }
+    }
+
     pub fn num_tabs(self) -> usize {
         1 + self.possible_tabs.len()
     }
 
     pub fn on_table(&self) -> bool {
         self.current_index == -1 || self.current_index - 1 > self.possible_tabs.len() as i16
+    }
+
+    pub fn switch_tab(&mut self, direction: TabDirection) {
+        match direction {
+            TabDirection::Forward => {
+                if self.current_index < self.possible_tabs.len() as i16 - 1 {
+                    self.current_index += 1
+                }
+            }
+            TabDirection::Backward => {
+                if self.current_index > -1 {
+                    self.current_index -= 1
+                }
+            }
+        }
     }
 }
 
